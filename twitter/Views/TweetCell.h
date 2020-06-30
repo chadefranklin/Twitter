63658cebd16e1332caca823df8e09849541bdbfd
Chad Franklin <chadfranklin@fb.com>
2020-06-30 15:18:32 -0400

    Added TweetCell TableView Model

---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,29 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by chadfranklin on 6/30/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tweet.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *displayNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetLabel;
+@property (weak, nonatomic) IBOutlet UIButton *favoriteButton;
+@property (weak, nonatomic) IBOutlet UIButton *retweetButton;
+
+
+@property (nonatomic, strong) Tweet *tweet;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
