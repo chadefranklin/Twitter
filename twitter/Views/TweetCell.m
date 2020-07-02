bbbcdcfa060a66d4af4e483d4c216bcde297f47c
Chad Franklin <chadfranklin@fb.com>
2020-07-02 14:12:24 -0400

    Added Ability To Tweet and Retweet

--- twitter/Views/TweetCell.m
+++ twitter/Views/TweetCell.m
@@ -54,15 +54,52 @@
     }
 }
 
-- (void)refreshData{
+- (IBAction)didTapRetweet:(id)sender {
+    if(self.tweet.retweeted){
+        self.tweet.retweeted = NO;
+        self.tweet.retweetCount -= 1;
+        
+        [self updateViews];
+        
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error unretweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    } else {
+        self.tweet.retweeted = YES;
+        self.tweet.retweetCount += 1;
+        
+        [self updateViews];
+        
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+}
+
+
+- (void)updateViews{
     if(self.tweet.favorited){
         [self.favoriteButton setTitle:[@(self.tweet.favoriteCount) stringValue] forState:UIControlStateSelected];
-        //[self.retweetButton setTitle:[@(self.tweet.retweetCount) stringValue] forState:UIControlStateSelected];
     } else {
         [self.favoriteButton setTitle:[@(self.tweet.favoriteCount) stringValue] forState:UIControlStateNormal];
-        //[self.retweetButton setTitle:[@(self.tweet.retweetCount) stringValue] forState:UIControlStateNormal];
+    }
+    if(self.tweet.retweeted){
+        [self.retweetButton setTitle:[@(self.tweet.retweetCount) stringValue] forState:UIControlStateSelected];
+    }else {
+        [self.retweetButton setTitle:[@(self.tweet.retweetCount) stringValue] forState:UIControlStateNormal];
     }
     self.favoriteButton.selected = self.tweet.favorited;
+    self.retweetButton.selected = self.tweet.retweeted;
 }
 
 @end
